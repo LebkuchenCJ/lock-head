7ad3e52a1d4638039c60c9ec4c89edbb10ca6c54
JonasImm <jonas.imm@gmail.com>
2020-08-24 15:48:56 +0200

    Add second prompt statement and write method

--- cli.js
+++ cli.js
@@ -3,6 +3,9 @@ const { async } = require("rxjs");
 
 const fs = require("fs").promises;
 
+const CHOICE_GET = "Read a password";
+const CHOICE_SET = "Set a password";
+
 const questions = [
   {
     type: "password",
@@ -10,19 +13,67 @@ const questions = [
     message: "What's your master password?",
   },
   {
+    type: "list",
+    name: "action",
+    message: "What do you want to do?",
+    choices: [CHOICE_GET, CHOICE_SET],
+  },
+];
+
+const questionsGet = [
+  {
     type: "input",
     name: "key",
     message: "Which password do you need?",
   },
 ];
 
-inquirer.prompt(questions).then(async (answers) => {
-  if (answers.password === "123") {
-    try {
-      const passwordsJSON = await fs.readFile("./passwords.json", "utf8");
-      const passwords = JSON.parse(passwordsJSON);
-      console.log(`Your ${answers.key} password is ${passwords[answers.key]}`);
-    } catch (error) {}
+const newQuestions = [
+  {
+    type: "input",
+    name: "key",
+    message: "Which account would like to add?",
+  },
+  {
+    type: "password",
+    name: "newPassword",
+    message: "Please enter the password to your new account.",
+  },
+];
+
+inquirer.prompt(questions).then(async (answersStart) => {
+  if (answersStart.password === "123") {
+    if (answersStart.action === CHOICE_GET) {
+      inquirer.prompt(questionsGet).then(async (answersGet) => {
+        try {
+          const passwordsJSON = await fs.readFile("./passwords.json", "utf8");
+          const passwords = JSON.parse(passwordsJSON);
+
+          if (answersGet.key in passwords) {
+            console.log(
+              `Your ${answersGet.key} password is ${passwords[answersGet.key]}`
+            );
+          } else {
+            console.log(`Your ${answersGet.key} Account does not exist.`);
+          }
+        } catch (error) {}
+      });
+    } else if (answersStart.action === CHOICE_SET) {
+      inquirer.prompt(newQuestions).then(async (newAnswers) => {
+        try {
+          const passwordsJSON = await fs.readFile("./passwords.json", "utf8");
+          const passwords = JSON.parse(passwordsJSON);
+          passwords[newAnswers.key] = newAnswers.newPassword;
+          const newObject = JSON.stringify(passwords);
+          fs.writeFile("./passwords.json", newObject, (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          });
+        } catch (error) {}
+      });
+    }
   } else {
     console.log("Master password is incorrect");
   }

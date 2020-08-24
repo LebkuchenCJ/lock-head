4693c9c950407cb4dfa3ee22943ff102e69b04ef
JonasImm <jonas.imm@gmail.com>
2020-08-24 12:36:51 +0200

    Password check function

--- cli.js
+++ cli.js
@@ -1,19 +1,29 @@
 const inquirer = require("inquirer");
+const { async } = require("rxjs");
+
+const fs = require("fs").promises;
 
 const questions = [
   {
     type: "password",
     name: "password",
-    message: "Whats your password?",
+    message: "What's your master password?",
   },
   {
     type: "input",
-    name: "name",
-    message: "Whats your name?",
+    name: "key",
+    message: "Which password do you need?",
   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(`Your password is ${answers.password}!`);
-  console.log(`Your name is ${answers.name}!`);
+inquirer.prompt(questions).then(async (answers) => {
+  if (answers.password === "123") {
+    try {
+      const passwordsJSON = await fs.readFile("./passwords.json", "utf8");
+      const passwords = JSON.parse(passwordsJSON);
+      console.log(`Your ${answers.key} password is ${passwords[answers.key]}`);
+    } catch (error) {}
+  } else {
+    console.log("Master password is incorrect");
+  }
 });

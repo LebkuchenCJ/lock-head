10a0394cede6465c36ef10a3ff51751a09a43c4d
JonasImm <jonas.imm@gmail.com>
2020-08-25 08:55:30 +0200

    Export components and added functions from leon

--- cli.js
+++ cli.js
@@ -1,4 +1,40 @@
-const inquirer = require("inquirer");
+const {
+  askStartQuestions,
+  askGetPasswordQuestions,
+  askSetPasswordQuestions,
+  CHOICE_GET,
+  CHOICE_SET,
+} = require("./lib/questions");
+const { readPassword } = require("./lib/passwords");
+
+async function main() {
+  const { masterPassword, action } = await askStartQuestions();
+
+  if (masterPassword === "123") {
+    console.log("Master Password is correct!");
+    if (action === CHOICE_GET) {
+      console.log("Now Get a password");
+      const { key } = await askGetPasswordQuestions();
+      try {
+        const password = await readPassword(key);
+        console.log(`Your ${key} password is ${password}`);
+      } catch (error) {
+        console.error("Something went wrong 😑");
+        // What to do now?
+      }
+    } else if (action === CHOICE_SET) {
+      console.log("Now Set a password");
+      const { key, password } = await askSetPasswordQuestions();
+      console.log(`New Password: ${key} = ${password}`);
+    }
+  } else {
+    console.log("Master Password is incorrect!");
+  }
+}
+
+main();
+
+/* const inquirer = require("inquirer");
 const { async } = require("rxjs");
 
 const fs = require("fs").promises;
@@ -41,6 +77,8 @@ const newQuestions = [
   },
 ];
 
+
+
 inquirer.prompt(questions).then(async (answersStart) => {
   if (answersStart.password === "123") {
     if (answersStart.action === CHOICE_GET) {
@@ -78,3 +116,4 @@ inquirer.prompt(questions).then(async (answersStart) => {
     console.log("Master password is incorrect");
   }
 });
+ */

cc3833736995f8f6fd1842fe5afb48a00c7750b0
JonasImm <jonas.imm@gmail.com>
2020-08-27 16:25:28 +0200

    Add read method

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
-const { writePassword } = require("./lib/passwords");
+const { writePassword, readPassword } = require("./lib/passwords");
 const { encrypt, decrypt } = require("./lib/crypto");
 
 const client = new MongoClient(process.env.MONGO_URL, {
@@ -20,6 +20,13 @@ async function main() {
 
   const database = client.db(process.env.MONGO_DB_NAME);
   const masterPassword = process.env.MASTER_PASSWORD;
+
+  app.get("/api/passwords/:name", async (request, response) => {
+    const { name } = request.params;
+    const password = await readPassword(name, database);
+    const decryptedPassword = decrypt(password, masterPassword);
+    response.status(200).send(decryptedPassword);
+  });
 
   app.post("/api/passwords", async (request, response) => {
     console.log("POST on /api/passwords");

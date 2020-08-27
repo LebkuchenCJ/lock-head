6ebf7f91e2f8e03b780c8f726faccda6d6ecb8fe
JonasImm <jonas.imm@gmail.com>
2020-08-27 11:58:46 +0200

    Add npm nodemon and post method

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+//const bodyParser = require("body-parser");
+
 const app = express();
 
-app.listen(3000, function () {
-  console.log("listening on 3000");
+const port = 3000;
+
+app.listen(port, function () {
+  console.log(`listening on ${port}`);
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get("/", (req, res) => {});
+
+app.post("/quotes", (req, res) => {
+  console.log("Hellooooooooooooooooo!");
 });

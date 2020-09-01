81a9584c863eb0d9c75350212a953f271a3beade
JonasImm <jonas.imm@gmail.com>
2020-09-01 18:04:49 +0200

    Add basic structre for new password method - no function

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
 import "./App.css";
 
+async function createPassword(name, password) {
+  const data = {
+    name,
+    password,
+  };
+  const response = await fetch("/api/passwords/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  });
+}
+
 async function login() {
   const data = {
     username: "jonas",
@@ -18,13 +32,14 @@ async function login() {
   alert(result);
 }
 
-async function getPasswords() {
-  const response = await fetch("/api/passwords/kebab");
+async function getPasswords(name) {
+  const response = await fetch(`/api/passwords/${name}`);
   const result = await response.text();
   alert(result);
 }
 function App() {
   const [passwordName, setPasswordName] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
   return (
     <div className="App">
       <header className="App-header">
@@ -36,6 +51,21 @@ function App() {
           />
         </label>
         <button onClick={() => getPasswords(passwordName)}>Get Password</button>
+        <label>
+          <input
+            value={passwordName}
+            placeholder="Enter account name"
+            onChange={(event) => setPasswordName(event.target.value)}
+          />
+          <input
+            value={passwordValue}
+            placeholder="Enter password"
+            onChange={(event) => setPasswordValue(event.target.value)}
+          />
+        </label>
+        <button onClick={() => createPassword(passwordName, passwordValue)}>
+          Create Password
+        </button>
       </header>
     </div>
   );

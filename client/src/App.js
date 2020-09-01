0b4656e35b1c158ac70e7f0f0ceb21d3f9012386
JonasImm <jonas.imm@gmail.com>
2020-09-01 16:37:25 +0200

    Add post / get function

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+async function login() {
+  const data = {
+    username: "jonas",
+    password: "123",
+  };
+
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(data),
+  });
+  const result = await response.text();
+  console.log(result);
+  return result;
+}
 function App() {
+  async function getPasswords() {
+    await login();
+    const response = await fetch("/api/passwords/kebab");
+    const result = await response.json();
+    console.log(result);
+    return result;
+  }
+  getPasswords();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>TEST</p>
       </header>
     </div>
   );
